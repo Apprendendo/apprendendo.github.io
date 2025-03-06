4989c86414e690eab6f01ce0d549ab5df12b300b
Camila <Camila.leite.oliveira@gmail.com>
2025-03-06 10:32:19 -0300

    Iniciando com a classe

---
+++ form.php
@@ -0,0 +1,18 @@
+[200~<!DOCTYPE html>
+<html>
+<head>
+  <title>Digite seu nome</title>
+</head>
+<body>
+ 
+  <h1>Digite seu nome:</h1>
+ 
+  <form action="exibir_nome.php" method="post">
+    <label for="nome">Nome:</label>
+    <input type="text" id="nome" name="nome" required><br><br>
+ 
+    <input type="submit" value="Enviar">
+  </form>
+ 
+</body>
+</html>
