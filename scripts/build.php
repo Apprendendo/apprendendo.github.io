ff76ee47072dc1ee3869bfd041d64198a8dbfb45
Camila <Camila.leite.oliveira@gmail.com>
2025-03-19 11:35:51 -0300

    Ajustando a pasta e inserindo a pagina index no script

--- scripts/build.php
+++ scripts/build.php
@@ -3,7 +3,9 @@
     require __DIR__ . '/../vendor/autoload.php';
 
     // Seu código PHP para gerar o conteúdo da página
-    $content = "<h1>Página gerada pelo PHP</h1><p>Data: " . date('Y-m-d H:i:s') . "</p>";
+    $content = "<h1>Página inicial das aulas de SW 1, em PHP</h1>
+        <h2>Prof. Camila Leite</h2>
+        <p>Data: " . date('d/m/Y H:i:s') . "</p>";
 
     // Salvar o conteúdo em um arquivo HTML
     file_put_contents(__DIR__ . '/../public/index.html', $content);
