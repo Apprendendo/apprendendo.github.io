fa18de5031cddefe1ff55ac421a30e461fa839ef
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2025-03-19 16:12:56 -0300

    Novamente atualizando o Index

--- scripts/index.php
+++ scripts/index.php
@@ -2,22 +2,10 @@
 <?php
     // Aviso: essa parte precisou ser chamada para montarmos a página no Github pages.
     require __DIR__ . '/../vendor/autoload.php';
-    
-    $titPag = "Inicio";
-    $lang = "pt-br";
-    $relStyle = "stylesheet";
-    $linkCss = "/assets/style.css";
+    require __DIR__ .'/assets/style.css';
 
 
     $content = "
-        <!DOCTYPE html>
-        <html lang=".$lang.">
-        <head>
-            <meta charset='UTF-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>".$titPag." - ".$nomePag."</title>
-            <link rel=".$relStyle." href=".$linkCss.">
-        </head>
         <body>
             <h1>Olá</h1>
             <p>Este é um exemplo de página com PHP.</p>
