f6c0f72d043557e869ac20390ad1304a2f367bd8
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2025-03-19 16:32:42 -0300

    add estilo interno

--- scripts/index.php
+++ scripts/index.php
@@ -14,7 +14,36 @@
             <meta charset=".$utf.">
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
             <title>".$titPag." - ".$nomePag."</title>
-            <link rel=".$relStyle." href=".$linkCss.">
+            <style>
+                @import url('https://fonts.googleapis.com/css?family=Muli:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');
+
+                *,
+                body {
+                    background-color: #010;
+                    font-family: 'Muli light', sans-serif;
+                    background-image: url(http://www.styleshout.com/templates/preview/Ceevee10/images/header-background.jpg)
+                }
+
+                h1,
+                p {
+                    text-align: center;
+                    color: #f1f1f1;
+                }
+
+                h1 {
+                    padding-top: 20%
+                }
+
+                hr {
+                    border: shade #E3E3E3;
+                    border-width: 1px 0 0;
+                    clear: both;
+                    /*margin: 11px 0 30px; */
+                    height: 0;
+                    width: 40%;
+                    position: center;
+                }
+            </style>
         </head>
         <body>
             <h1>Olá</h1>
@@ -25,7 +54,6 @@
 
     // Salvar o conteúdo em um arquivo HTML
     file_put_contents(__DIR__ . '/../public/index.html', $content);
-    file_put_contents(__DIR__. '/../public/assets/style.css', $linkCss);
 
     echo "Página compilada com sucesso!\n";
 ?>
