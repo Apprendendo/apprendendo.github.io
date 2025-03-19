e9f4f6e31df4f1d06bc126a365b049326cb67b62
Camila <Camila.leite.oliveira@gmail.com>
2025-03-19 08:23:47 -0300

    Criando a pagina de login para a AAE 1.1

---
+++ public/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Aula SW 1</title>
+</head>
+<body>
+    <!-- Agora é com os alunos -->
+    <!-- Crie um formulario, com os campos de e-mail e senha -->
+    <!-- Use os botões de cancelar e confirmar o login -->
+    <!-- Busque redirecionar ao index.php personalizado -->
+    <!-- Tente fazer uma validação com uma conta ficticia -->
+    <!-- Exemplo: e-mail = "calecmo@gmail.com" senha="123456" -->
+</body>
+</html>
